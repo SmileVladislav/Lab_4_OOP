478bf709b53a975658b700b25a0ee335b0cc5878
Vladislav <143926973+SmileVladislav@users.noreply.github.com>
2026-03-30 00:54:47 +0300

    Word.java

--- Word.java
+++ Word.java
@@ -1,2 +1,11 @@
+import java.util.Arrays;
+
 public class Word {
-}
+    private final Letter[] letters;
+
+    public Word(String wordStr) {
+        this.letters = new Letter[wordStr.length()];
+        for (int i = 0; i < wordStr.length(); i++) {
+            this.letters[i] = new Letter(wordStr.charAt(i));
+        }
+   }

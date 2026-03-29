c1aecae18f24ba92c66cce28a8f3a8e16eb3bd75
Vladislav <143926973+SmileVladislav@users.noreply.github.com>
2026-03-30 00:40:44 +0300

    Letter.java

--- Letter.java
+++ Letter.java
@@ -1,2 +1,20 @@
+import java.util.Objects;
 public class Letter {
+  private final char symbol;
+  
+  public Letter(char symbol){
+    this.symbol = symbol;
 }
+@Override 
+public String toString() {
+  return String.valueOf(symbol);
+}
+@Override 
+public boolean equals(Object o) {
+  if (this == o) return true;
+  if (o == null || getClass() != o.getClass()) return false;
+  Letter letter = (letter) o;
+  return symbol == letter.symbol;
+}  
+
+  

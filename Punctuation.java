00f3b9a47473145f33b36c8c45efec2ab28fb112
Vladislav <143926973+SmileVladislav@users.noreply.github.com>
2026-03-30 13:25:53 +0300

    Punctuation.java

--- Punctuation.java
+++ Punctuation.java
@@ -1,2 +1,22 @@
-public class Punctuation {
+import java.util.Objects;
+
+public class Punctuation implements SentenceElement {
+    private final char symbol;
+
+    public Punctuation(char symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(symbol);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Punctuation that = (Punctuation) o;
+        return symbol == that.symbol;
+    }
 }

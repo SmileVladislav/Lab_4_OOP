accca03d344f249f0541152e0dfef79f4fd75e46
Vladislav <143926973+SmileVladislav@users.noreply.github.com>
2026-03-30 10:48:58 +0300

    Word.java

--- Word.java
+++ Word.java
@@ -8,4 +8,12 @@ public class Word {
         for (int i = 0; i < wordStr.length(); i++) {
             this.letters[i] = new Letter(wordStr.charAt(i));
         }
-   }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Letter l : letters) sb.append(l.toString());
+        return sb.toString();
+    }
+}

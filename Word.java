e7c175e076906316e1773807373ba3d3eb0e1da2
Vladislav <143926973+SmileVladislav@users.noreply.github.com>
2026-04-21 20:22:52 +0300

    Word.java

--- Word.java
+++ Word.java
@@ -1,29 +1,15 @@
-import java.util.Arrays;
-
-public class Word implements SentenceElement {
-    private final Letter[] letters;
-
+public class Word {
+    private Letter[] letters;
     public Word(String s) {
-        this.letters = new Letter[s.length()];
+        letters = new Letter[s.length()];
         for (int i = 0; i < s.length(); i++) {
-            this.letters[i] = new Letter(s.charAt(i));
+            letters[i] = new Letter(s.charAt(i));
         }
     }
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (Letter l : letters) {
-            sb.append(l.toString());
-        }
+        for (Letter l : letters) sb.append(l.toString());
         return sb.toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Word word = (Word) o;
-        return Arrays.equals(letters, word.letters);
     }
 }
